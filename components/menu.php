81e7e02f2eb896df413833e12d01980f605d5004
utpsantiagohernandez <santigo.hernandez@utponiente.edu.mx>
2021-10-04 22:31:25 -0500

    Version inicial

---
+++ components/menu.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand mb-0 h1" href="index.html">Contactos</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="me-auto"></div>
+            <div class="flex">
+                <ul class="navbar-nav  mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="favoritos.php">Favoritos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="acercade.php">Acerca de</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
